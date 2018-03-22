7443b1ccd82ea4f0a872c4280381316ccee034ff
god <164798848@qq.com>
2018-03-22 23:54:33 +0800

    code

--- _release.sh
+++ _release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-NAME="neuron-oauth-authorize-web" \
-VERSION="v1.0.0" \
+NAME=neuron-oauth-authorize-web \
+VERSION=v1.0.0 \
 PORT=3003 \
-PUBLIC_URL=/web/oauth/authorize \
+REACT_APP_WEB_PATH=/web/oauth/authorize \
 neuron-react-release.sh
